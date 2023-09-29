b91df1301eb1a6699505b8fea80bb9e1634371db
root <root@ASUS.localdomain>
2023-09-30 03:27:50 +0500

    some hope

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: false,
-
-    timeline: {
-      enabled: false
-    }
-  },
-  modules: ["@nuxt/content", "nuxt-og-image"],
+  modules: ["@nuxt/content"],
   css: ["@/assets/css/main.css"],
   router: {
     linkActiveClass: "active-route",
@@ -15,16 +8,5 @@ export default defineNuxtConfig({
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
-  },
-  site: {
-    url: 'https://5611-94-158-61-228.ngrok-free.app/',
-  },
-
-
-  
-  runtimeConfig: {
-    public: {
-      siteUrl: 'https://5611-94-158-61-228.ngrok-free.app/'
-    }
   }
 });
