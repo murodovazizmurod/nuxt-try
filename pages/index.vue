11962769c13f6b2325969f7c7160a42ee2f250b1
Murodov Azizmurod <murodovazizmurod@gmail.com>
2023-12-21 23:02:04 +0500

    added loading screen ✨

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,6 @@
       <p>
         > my name is azizmurod; <br>
         > <br>
-<<<<<<< HEAD
-=======
-        > TSUE'27 (unfortunately) <br>
->>>>>>> 8aab0cdd2fe27b58287d59d1feae6c7352e62726
         ><br>
         > enthusiastic self-taught developer from <a href="https://en.wikipedia.org/wiki/Navoiy"><img alt='Location Icon' draggable="false" style="width:15px;height:15px;margin-bottom: -3px;margin-right: 2px;" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAACXBIWXMAAAsTAAALEwEAmpwYAAABvUlEQVR4nLXWu2oVURTG8aNJsFAwitdgJYiFIF7w8gQWURBfIE1UrERtvKQLUYSAEkWMCuobKAqCZXKCWkm0FyUqiqIpU/5kwyqGzTnMnpM4zTDfrLX+s9f6Zs+0WgUHVuMozuJynI8kvSS/pHgquIB5TONmnD+EfrpnGNbhJWawv0vMAcziOdb2soIXeIz+mth+PAlQ+YqiRTN1gAzUxmirwSoW8hahDydwEcfTdXb/IL5gVQkkuWg+0zbgXazuVszhLQazuI84XNqq6Ux7iNuZNtUl7kwJZAwTmfYd2zNtKOmZdh3XSiAX0lP2CLmD8yWQk3iVaQ+q4DRc3O3QrtfJHCWQ9IR/MFDRBmPQ7Rj8HN5UB481WMSWWkgkJCcd62Dh4bDwcAcLJ3u3iwCRcCVvRUHOU1xqkrADf7GxMH5bxJe1qpKY9qOxwtgbuNcIEIl78APra+I24zd2NoZEgUeYrIm5n+8GTSFb8Qu7u9zfi59pb+sZEoXOxTvRl+kDeI+RZQEqb3f6Ql7N9HE8WzYgs+g37IvrQ/iKTSsGicKn8Am78Dl9uFYUUAFNYin9sfwXQEDS/jWSm6Du+AcJmFc1+geZBgAAAABJRU5ErkJggg==">navoi, uzbekistan</a><br>
         > breathes; walks; talks;<br>
